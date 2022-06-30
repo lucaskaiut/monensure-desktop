7a853394bc5ac13f9d16c6647856075869777d25
Kaiut <lucas.kaiut@gmail.com>
2022-06-30 20:39:33 -0300

    added access token to env

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: {
-    Authorization: 'Bearer 1|HcLf3d4xoXWGjOaygq1w8bxiaFdYscc2naadJOlP'
+    Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
   }
 });
 
